81cacb57f2568f55ba10b2d908ed2389891cb9fa
ppicheta <piotr.picheta@arcelormittal.com>
2023-09-22 22:58:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ st.header('Our Team')
 col1, empty_col1, col2, empty_col2, col3 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
 
 with col1:
-    for index, row in df[:5].iterrows():
+    for index, row in df[:4].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
         st.image('images/' + row['image'])
 
 with col2:
-    for index, row in df[5:9].iterrows():
+    for index, row in df[4:8].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
         st.image('images/' + row['image'])
 
 with col3:
-    for index, row in df[9:].iterrows():
+    for index, row in df[8:].iterrows():
         st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row['role'])
         st.image('images/' + row['image'])
